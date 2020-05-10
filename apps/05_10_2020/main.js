599381910578390158c86d08facd493a6a7a40af
konradgnat <konradmgnat@gmail.com>
2020-05-10 13:33:06 -0500

    threejs
    ;

--- apps/05_10_2020/main.js
+++ apps/05_10_2020/main.js
@@ -14,4 +14,29 @@ var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+// add IcosahedronGeometry
+
+var icosahedronGeometry = new THREE.IcosahedronGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var icosahedron = new THREE.Mesh( icosahedronGeometry, material );
+scene.add( icosahedron );
+// icosahedron.position.set(100, 100, 100);
+
+
 camera.position.z = 5;
+
+
+function animate() {
+  requestAnimationFrame( animate );
+  
+  icosahedron.rotation.x += 0.01;
+  icosahedron.rotation.y += 0.01;
+
+  // demo cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+animate();
+
